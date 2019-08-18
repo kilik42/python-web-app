12a24e4a1665dfa009dc84973b1a8fceb4cad1f2
marvin evins <maevins@cps.edu>
2019-08-18 12:26:00 -0400

    added database

--- app.py
+++ app.py
@@ -6,9 +6,33 @@ app = Flask(__name__)
 ENV = 'dev'
 
 if ENV == 'dev':
-
+    app.debug = True
+    app.config['SQLAlCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/lexus'
 else:
-    
+    app.debug = False
+    app.config['SQLAlCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+
+db = SQLAlchemy(app)
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique = True)
+    dealer = db.Column(db.String(200))
+    rating = db.Column(db.Integer)
+    comments = db.Column(db.Text())
+
+def __init__(self, customer, dealer, rating, comments):
+    self.customer = customer
+    self.dealer = dealer
+    self.rating = rating
+    self.comments = comments
+
+
+
 
 
 @app.route('/')
